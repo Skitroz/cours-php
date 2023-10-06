303557d433a9eb2aaa57b661f63039af93a9e118
Skitroz <matheo08batelier@gmail.com>
2023-10-06 13:55:59 +0200

    .

--- src/crud.php
+++ src/crud.php
@@ -3,16 +3,51 @@ require_once "./src/dbConnect.php";
 require_once "./src/toolkit.php";
 
 //fonction getAll
+function getAll($table, $id){
+    global $connection;
+    $statement = $connection->query("SELECT * FROM $table WHERE $id");
+    $data = $statement->fetchAll(PDO::FETCH_ASSOC);
+    dd($data);
+}
 // getAll("contacts", 1);
 
 //fonction getById
+function getId($table, $name, $surname){
+    global $connection;
+    $statement = $connection->prepare("SELECT * FROM $table WHERE `name` = :name AND `surname` = :surname");
+    $statement->bindParam(':name', $name);
+    $statement->bindParam(':surname', $surname);
+    $statement->execute();
+    $data = $statement->fetchAll(PDO::FETCH_ASSOC);
+    dd($data);
+}
 // getId("contacts", "Batelier", "Mathéo");
 
-//fonction create 
+//fonction create
+function create($table, $name, $surname){
+    global $connection;
+    $statement = $connection->prepare("INSERT INTO $table (`name`, `surname`, `status`) VALUES (:name, :surname, 'online') ");
+    $statement->bindParam(':name', $name);
+    $statement->bindParam(":surname", $surname);
+    $statement->execute();
+}
 // create("contacts", "Couder", "Bastien");
 
 //fonction delete
+function delete($table, $id){
+    global $connection;
+    $statement = $connection->prepare("DELETE FROM $table WHERE id = ?");
+    $statement->bindParam(1, $id);
+    $statement->execute();
+}
 // delete("contacts", 13);
 
 //fonction update
-update("contacts", 1, "online");
+function update($table, $id, $status){
+    global $connection;
+    $statement = $connection->prepare("UPDATE $table SET `status` = ? WHERE id = ?");
+    $statement->bindParam(1, $status);
+    $statement->bindParam(2, $id);
+    $statement->execute();
+}
+// update("contacts", 1, "online");
